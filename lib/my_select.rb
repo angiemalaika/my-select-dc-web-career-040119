5d3731eba38f413fb79ebd715f1f7232bf101db5
Angela Mumba <malaika.mumba@gmail.com>
2019-03-29 18:54:09 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,16 @@
 def my_select(collection)
  # your code here!
 end
+
+def my_select(collection) 
+count = 0
+result = []
+
+while count < collection.size
+  if yield(collection[count]) == true
+  result.push(collection[count])
+end
+  count+=1
+end
+result
+end
